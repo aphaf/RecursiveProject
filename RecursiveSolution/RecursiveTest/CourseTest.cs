7f590ce79473e7b010fb2685694728b5e1f0df31
aphaf <aphaf0029@gmail.com>
2023-01-14 19:30:16 -0500

    Edited mockdata in tests

--- RecursiveSolution/RecursiveTest/CourseTest.cs
+++ RecursiveSolution/RecursiveTest/CourseTest.cs
@@ -12,27 +12,99 @@ namespace RecursiveTest
     {
         [Fact]
         public void ShouldFindAllCoursePreReqs()
-        {    
-            Course main = new Course(1, "452");
-            Course c450 = new Course(2, "450");
-            Course c353 = new Course(3, "353");
-            Course c352 = new Course(4, "352");
-            Course c351 = new Course(5, "351");
+        {
+            List<Course> mockCourses = CreateMockCourses();
 
-            c353.PreReqCourses.Add(new PreReq { Course = c353, PreReqCourse = c352 });
-            c353.PreReqCourses.Add(new PreReq { Course = c353, PreReqCourse = c351 });
-
-            c450.PreReqCourses.Add(new PreReq { Course = c450, PreReqCourse = c353 });
-
-            main.PreReqCourses.Add(new PreReq { Course = main, PreReqCourse = c450 });
-            main.PreReqCourses.Add(new PreReq { Course = main, PreReqCourse = c353 });
-
-            List<Course> preReqList = main.FindAllPreReqs(main, new List<Course>());
+            //List<Course> preReqList = course.FindAllPreReqs(course, new List<Course>());
 
             int expected = 4;
-            int actual = preReqList.Count;
+            int actual = mockCourses.Last().FindAllPreReqs(mockCourses.Last(), new List<Course>()).Count;
 
             Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldNotFindCoursePreReq()
+        {
+            List<Course> mockCourses = CreateMockCourses();
+
+            //List<Course> preReqList = course.FindAllPreReqs(course, new List<Course>());
+
+            int expected = 0;
+            int actual = mockCourses.First().FindAllPreReqs(mockCourses.First(), new List<Course>()).Count;
+
+            Assert.Equal(expected, actual);
+        }
+
+        public List<Course> CreateMockCourses()
+        {
+            List<Course> mockCourses = new List<Course>();
+
+            Course course = new Course
+            {
+                CourseID = 1,
+                Name = "351",
+            };
+            mockCourses.Add(course);
+
+            course = new Course
+            {
+                CourseID = 3,
+                Name = "353",
+                PreReqCourses = new List<PreReq>
+                {
+                    new PreReq { PreReqCourse = course },
+                    new PreReq { PreReqCourse = new Course(2, "352") }
+                }
+            };
+            List<Course> preReqsFor452 = new List<Course> { course };
+            mockCourses.Add(course);
+
+            course = new Course
+            {
+                CourseID = 4,
+                Name = "450",
+                PreReqCourses = new List<PreReq>
+                {
+                    new PreReq { PreReqCourse = course }
+                }
+            };
+            preReqsFor452.Add(course);
+            mockCourses.Add(course);
+
+            course = new Course 
+            { 
+                CourseID = 5,
+                Name = "452",
+                PreReqCourses = new List<PreReq> { new PreReq { PreReqCourse = preReqsFor452[0] }, new PreReq { PreReqCourse = preReqsFor452[1] } }
+            };
+            mockCourses.Add(course);
+
+            return mockCourses;
+
+
+
+            //Course c452 = new Course(1, "452");
+            //c452.PreReqCourses.Add(new PreReq { Course = c452, PreReqCourse = c353 });
+            //c452.PreReqCourses.Add(new PreReq { Course = c452, PreReqCourse = c353 });
+
+
+            //Course c353 = new Course(3, "353");
+            //Course c352 = new Course(4, "352");
+            //Course c351 = new Course(5, "351");
+
+            //c353.PreReqCourses.Add(new PreReq { Course = c353, PreReqCourse = c352 });
+            //c353.PreReqCourses.Add(new PreReq { Course = c353, PreReqCourse = c351 });
+
+
+            //Course c450 = new Course(2, "450");
+            //c450.PreReqCourses.Add(new PreReq { Course = c450, PreReqCourse = c353 });
+
+
+            //Course main = new Course(1, "452");
+            //main.PreReqCourses.Add(new PreReq { Course = main, PreReqCourse = c450 });
+            //main.PreReqCourses.Add(new PreReq { Course = main, PreReqCourse = c353 });
+
         }
     }
 }
--- RecursiveSolution/RecursiveTest/EmployeeTest.cs
+++ RecursiveSolution/RecursiveTest/EmployeeTest.cs
@@ -12,33 +12,84 @@ namespace RecursiveTest
         [Fact]
         public void ShouldFindEmployeeSupervisors()
         {
+            List<Employee> mockEmployees = CreateMockEmployees();
+
+            //List<Employee> supervisorList = employee.FindAllSupervisors(employee);
+
+            int expected = 3;
+            int actual = mockEmployees.Last().FindAllSupervisors(mockEmployees.Last()).Count;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldFindNoEmployeeSupervisors()
+        {
+            List<Employee> mockEmployees = CreateMockEmployees();
+
+            int expected = 0;
+            int actual = mockEmployees.First().FindAllSupervisors(mockEmployees.First()).Count;
+
+            Assert.Equal(expected, actual);
+        }
+
+        public List<Employee> CreateMockEmployees()
+        {
+            List<Employee> mockEmployees = new List<Employee>();
+
             Employee employee = new Employee
             {
                 EmployeeID = 1,
-                Name = "Penguin",
-                Supervisor = new Employee
-                {
-                    EmployeeID = 2,
-                    Name = "Pig",
-                    Supervisor = new Employee
-                    {
-                        EmployeeID = 3,
-                        Name = "Giraffe",
-                        Supervisor = new Employee
-                        {
-                            EmployeeID = 4,
-                            Name = "Flamingo"
-                        }
-                    }
-                }
+                Name = "Flamingo"
             };
+            mockEmployees.Add(employee);
 
-            List<Employee> supervisorList = employee.FindAllSupervisors(employee);
+            employee = new Employee
+            {
+                EmployeeID = 2,
+                Name = "Giraffe",
+                Supervisor = employee
+            };
+            mockEmployees.Add(employee);
 
-            int expected = 3;
-            int actual = supervisorList.Count();
+            employee = new Employee
+            {
+                EmployeeID = 2,
+                Name = "Pig",
+                Supervisor = employee
+            };
+            mockEmployees.Add(employee);
 
-            Assert.Equal(expected, actual);
+            employee = new Employee
+            {
+                EmployeeID = 2,
+                Name = "Penguin",
+                Supervisor = employee
+            };
+            mockEmployees.Add(employee);
+
+            //Employee employee = new Employee
+            //{
+            //    EmployeeID = 1,
+            //    Name = "Penguin",
+            //    Supervisor = new Employee
+            //    {
+            //        EmployeeID = 2,
+            //        Name = "Pig",
+            //        Supervisor = new Employee
+            //        {
+            //            EmployeeID = 3,
+            //            Name = "Giraffe",
+            //            Supervisor = new Employee
+            //            {
+            //                EmployeeID = 4,
+            //                Name = "Flamingo"
+            //            }
+            //        }
+            //    }
+            //};
+
+            return mockEmployees;
         }
     }
 }
